4b3a12b39fc4fa7599f9d09ecd6d785a4ffc089d
Boris Kimmina <boris.kimmina@zalando.de>
2017-09-22 10:06:45 +0200

    Include chrome profile in package

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup
 
-setup(name='chrome_remote_shell',
-      version='0.1',
-      description='Client for remote debugging Google Chrome',
-      url='https://github.com/tempelkim/chrome-remote-shell',
-      author='Boris Kimmina',
-      author_email='kim@kimmina.net',
-      license='MIT',
-      packages=['chromeremote'],
-      install_requires=[
-          'websocket-client',
-      ],
-      zip_safe=False)
+setup(
+        name='chrome_remote_shell',
+        version='0.1',
+        description='Client for remote debugging Google Chrome',
+        url='https://github.com/tempelkim/chrome-remote-shell',
+        author='Boris Kimmina',
+        author_email='kim@kimmina.net',
+        license='MIT',
+        packages=['chromeremote'],
+        package_data={'': ['*.gz']},
+        install_requires=[
+            'websocket-client',
+        ],
+        zip_safe=False,
+)
